4e49aa5ef7b909f36ab5a5ce8d21aee70bcee5f9
kentish121-afk <kentish121@googlemail.com>
2026-07-26 00:34:33 +0100

    Add Google Play Services Location dependency for ACCESS_FINE_LOCATION support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,9 @@ dependencies {
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
+    // Location (for ACCESS_FINE_LOCATION / nearby stops)
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
